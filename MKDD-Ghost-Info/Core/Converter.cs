742459e8f3b9695e0a363086f2ce19d206c6d7b5
SwareJonge <olaf23okken@gmail.com>
2020-03-09 23:10:24 +0100

    Added Live replay lua stuff

---
+++ MKDD-Ghost-Info/Core/Converter.cs
@@ -0,0 +1,476 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKDD_Ghost_Info.Core
+{
+    class ConverterFunctions
+    {
+        public string GetCharInfo(byte x)
+        {
+            if (x == 1)
+            {
+                return "Baby Mario";
+            }
+            if (x == 2)
+            {
+                return "Baby Luigi";
+            }
+            if (x == 3)
+            {
+                return "Patroopa";
+            }
+            if (x == 4)
+            {
+                return "Koopa";
+            }
+            if (x == 5)
+            {
+                return "Peach";
+            }
+            if (x == 6)
+            {
+                return "Daisy";
+            }
+            if (x == 7)
+            {
+                return "Mario";
+            }
+            if (x == 8)
+            {
+                return "Luigi";
+            }
+            if (x == 9)
+            {
+                return "Wario";
+            }
+            if (x == 10)
+            {
+                return "Waluigi";
+            }
+            if (x == 11)
+            {
+                return "Yoshi";
+            }
+            if (x == 12)
+            {
+                return "Birdo";
+            }
+            if (x == 13)
+            {
+                return "Donkey Kong";
+            }
+            if (x == 14)
+            {
+                return "Diddy Kong";
+            }
+            if (x == 15)
+            {
+                return "Bowser";
+            }
+            if (x == 16)
+            {
+                return "Bowser Jr.";
+            }
+            if (x == 17)
+            {
+                return "Toad";
+            }
+            if (x == 18)
+            {
+                return "Toadette";
+            }
+            if (x == 19)
+            {
+                return "King Boo";
+            }
+            if (x == 20)
+            {
+                return "Petey Piranha";
+            }
+            else
+            {
+                return "None";
+            }
+        }
+
+        public string GetKartID(byte x)
+        {
+
+            if (x == 0)
+            {
+                return "Red Fire";
+            }
+            if (x == 1)
+            {
+                return "DK Jumbo";
+            }
+            if (x == 2)
+            {
+                return "Turbo Yoshi";
+            }
+            if (x == 3)
+            {
+                return "Koopa Dasher";
+            }
+            if (x == 4)
+            {
+                return "Heart Coach";
+            }
+            if (x == 5)
+            {
+                return "Goo-Goo Buggy";
+            }
+            if (x == 6)
+            {
+                return "Wario Car";
+            }
+            if (x == 7)
+            {
+                return "Koopa King";
+            }
+            if (x == 8)
+            {
+                return "Green Fire";
+            }
+            if (x == 9)
+            {
+                return "Barrel Train";
+            }
+            if (x == 10)
+            {
+                return "Turbo Birdo";
+            }
+            if (x == 11)
+            {
+                return "Para Wing";
+            }
+            if (x == 12)
+            {
+                return "Bloom Coach";
+            }
+            if (x == 13)
+            {
+                return "Rattle Buggy";
+            }
+            if (x == 14)
+            {
+                return "Waluigi Racer";
+            }
+            if (x == 15)
+            {
+                return "Bullet Blaster";
+            }
+            if (x == 16)
+            {
+                return "Toad Kart";
+            }
+            if (x == 17)
+            {
+                return "Toadette Kart";
+            }
+            if (x == 18)
+            {
+                return "Boo Pipes";
+            }
+            if (x == 19)
+            {
+                return "Piranha Pipes";
+            }
+            if (x == 20)
+            {
+                return "Parade Kart";
+            }
+            else
+            {
+                return "Unknown ID";
+            }
+        }
+
+        public string GetCourseID(byte x)
+        {
+
+            if (x == 0x21)
+            {
+                return "Baby Park";
+            }
+            if (x == 0x22)
+            {
+                return "Peach Beach";
+            }
+            if (x == 0x23)
+            {
+                return "Daisy Cruiser";
+            }
+            if (x == 0x24)
+            {
+                return "Luigi Circuit";
+            }
+            if (x == 0x25)
+            {
+                return "Mario Circuit";
+            }
+            if (x == 0x26)
+            {
+                return "Yoshi Circuit";
+            }
+            if (x == 0x28)
+            {
+                return "Mushroom Bridge";
+            }
+            if (x == 0x29)
+            {
+                return "Mushroom City";
+            }
+            if (x == 0x2A)
+            {
+                return "Waluigi Stadium";
+            }
+            if (x == 0x2B)
+            {
+                return "Wario Colosseum";
+            }
+            if (x == 0x2C)
+            {
+                return "Dino Dino Jungle";
+            }
+            if (x == 0x2D)
+            {
+                return "DK Mountain";
+            }
+            if (x == 0x2F)
+            {
+                return "Bowser\'s Castle";
+            }
+            if (x == 0x31)
+            {
+                return "Rainbow Road";
+            }
+            if (x == 0x32)
+            {
+                return "Waluigi Racer";
+            }
+            if (x == 0x33)
+            {
+                return "Dry Dry Desert";
+            }
+            else
+            {
+                return "Sherbet Land";
+            }
+        }
+        public byte ConvertAnalogY(int x)
+        {
+            if (x == 3)
+            {
+                return 197;
+            }
+            if (x == 2)
+            {
+                return 179;
+            }
+            if (x == 1)
+            {
+                return 161;
+            }
+            if (x == 7)
+            {
+                return 95;
+            }
+            if (x == 6)
+            {
+                return 77;
+            }
+            if (x == 5)
+            {
+                return 59;
+            }
+            else
+            {
+                return 128;
+            }
+        }
+
+        public byte ConvertAnalogX(int x)
+        {
+            if (x == 0x88)
+            {
+                return 53;
+            }
+            if (x == 0x90)
+            {
+                return 57;
+            }
+            if (x == 0x98)
+            {
+                return 61;
+            }
+            if (x == 0xA0)
+            {
+                return 65;
+            }
+            if (x == 0xA8)
+            {
+                return 69;
+            }
+            if (x == 0xB0)
+            {
+                return 73;
+            }
+            if (x == 0xB8)
+            {
+                return 77;
+            }
+            if (x == 0xC0)
+            {
+                return 81;
+            }
+            if (x == 0xC8)
+            {
+                return 85;
+            }
+            if (x == 0xD0)
+            {
+                return 89;
+            }
+            if (x == 0xD8)
+            {
+                return 93;
+            }
+            if (x == 0xE0)
+            {
+                return 97;
+            }
+            if (x == 0xE8)
+            {
+                return 101;
+            }
+            if (x == 0xF0)
+            {
+                return 105;
+            }
+            if (x == 0xF8)
+            {
+                return 109;
+            }
+            if (x == 0x08)
+            {
+                return 147;
+            }
+            if (x == 0x10)
+            {
+                return 151;
+            }
+            if (x == 0x18)
+            {
+                return 155;
+            }
+            if (x == 0x20)
+            {
+                return 159;
+            }
+            if (x == 0x28)
+            {
+                return 163;
+            }
+            if (x == 0x30)
+            {
+                return 167;
+            }
+            if (x == 0x38)
+            {
+                return 171;
+            }
+            if (x == 0x40)
+            {
+                return 175;
+            }
+            if (x == 0x48)
+            {
+                return 179;
+            }
+            if (x == 0x50)
+            {
+                return 183;
+            }
+            if (x == 0x58)
+            {
+                return 187;
+            }
+            if (x == 0x60)
+            {
+                return 191;
+            }
+            if (x == 0x68)
+            {
+                return 195;
+            }
+            if (x == 0x70)
+            {
+                return 199;
+            }
+            if (x == 0x78)
+            {
+                return 203;
+            }
+            else
+            {
+                return 128;
+            }
+        }
+
+        public byte checkIfAPressed(byte x)
+        {
+            if ((x & (1 << 0)) != 0)
+            {
+                return 1;
+            }
+            return 0;
+        }
+        public byte checkIfBPressed(byte x)
+        {
+            if ((x & (1 << 1)) != 0)
+            {
+                return 1;
+            }
+            return 0;
+        }
+        public byte checkIfXPressed(byte x)
+        {
+            if ((x & (1 << 2)) != 0 || (x & (1 << 3)) != 0)
+            {
+                return 1;
+            }
+            return 0;
+        }
+        public byte checkIfRPressed(byte x)
+        {
+            if ((x & (1 << 5)) != 0)
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        public byte checkIfLPressed(byte x)
+        {
+            if ((x & (1 << 4)) != 0)
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        public byte checkIfZPressed(byte x)
+        {
+            if ((x & (1 << 6)) != 0)
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+    }
+}
---
+++ MKDD-Ghost-Info/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MKDD_Ghost_Info.Core;
+
+namespace MKDD_Ghost_Info
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Saved Ghost Data|*.gci;*.ght|Input Data only|*.bin";
+            openFileDialog1.FileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if(openFileDialog1.FileName.ToString().EndsWith(".gci"))
+                {
+                    ConverterFunctions C = new ConverterFunctions();
+                    byte[] InputDataLengthArray = File.ReadAllBytes(openFileDialog1.FileName).Skip(0x148E).Take(2).ToArray();
+                    ushort InputDataLength = BitConverter.ToUInt16(new byte[2] { InputDataLengthArray[1], InputDataLengthArray[0] }, 0);
+                    int Length = InputDataLength * 2;
+                    byte[] FinishTime = File.ReadAllBytes(openFileDialog1.FileName).Skip(0x146B).Take(8).ToArray();
+                    var str = Encoding.Default.GetString(FinishTime);
+                    byte[] IDs = File.ReadAllBytes(openFileDialog1.FileName).Skip(0x1480).Take(4).ToArray();
+                    dataGridView1.Rows.Clear();
+                    string path = @"mkdd_input_reader_ghost.lua";
+                    using (StreamWriter sw = File.CreateText(path))
+                    {
+                        sw.WriteLine("local mkdd_input_reader_ghost = { -- { StickX(converted), StickY(converted), A, B, R, L, X, Z}");
+                        for (int i = 0; i <= Length; i += 2)
+                        {
+                            byte[] Inputs = File.ReadAllBytes(openFileDialog1.FileName).Skip(0x14A8 + i).Take(2).ToArray();
+                            var AnalogStickY = (Inputs[0] & 7);
+                            var AnalogStickX = Inputs[0] - AnalogStickY;
+                            byte ABLR = Inputs[1];
+                            dataGridView1.Rows.Add(i / 2
+                                , C.ConvertAnalogX(AnalogStickX)
+                                , C.ConvertAnalogY(AnalogStickY)
+                                , C.checkIfAPressed(Inputs[1])
+                                , C.checkIfBPressed(Inputs[1])
+                                , C.checkIfRPressed(Inputs[1])
+                                , C.checkIfLPressed(Inputs[1])
+                                , C.checkIfXPressed(Inputs[1])
+                                , C.checkIfZPressed(Inputs[1]));
+
+                            sw.WriteLine("{"
+                            + C.ConvertAnalogX(AnalogStickX) + ", "
+                            + C.ConvertAnalogY(AnalogStickY) + ", "
+                            + C.checkIfAPressed(Inputs[1]) + ", "
+                            + C.checkIfBPressed(Inputs[1]) + ", "
+                            + C.checkIfRPressed(Inputs[1]) + ", "
+                            + C.checkIfLPressed(Inputs[1]) + ", "
+                            + C.checkIfXPressed(Inputs[1]) + ", "
+                            + C.checkIfZPressed(Inputs[1]) + "},");
+                        }
+                        sw.WriteLine("}\nreturn mkdd_input_reader_ghost");
+                    }
+                    label3.Text = str;
+                    label4.Text = InputDataLength.ToString();
+                    label8.Text = C.GetCharInfo(IDs[0]);
+                    label9.Text = C.GetCharInfo(IDs[1]);
+                    label10.Text = C.GetKartID(IDs[2]);
+                    label11.Text = C.GetCourseID(IDs[3]);
+                }
+                else if (openFileDialog1.FileName.ToString().EndsWith(".ght"))
+                {                    
+                        ConverterFunctions C = new ConverterFunctions();
+                        byte[] InputDataLengthArray = File.ReadAllBytes(openFileDialog1.FileName).Skip(0xE).Take(2).ToArray();
+                        ushort InputDataLength = BitConverter.ToUInt16(new byte[2] { InputDataLengthArray[1], InputDataLengthArray[0] }, 0);
+                        int Length = InputDataLength * 2;
+                        byte[] FinishTimeArray = File.ReadAllBytes(openFileDialog1.FileName).Skip(0x8).Take(4).ToArray();
+                        uint FinishTime = BitConverter.ToUInt32(new byte[4] { FinishTimeArray[3], FinishTimeArray[2], FinishTimeArray[1], FinishTimeArray[0] }, 0);
+                        TimeSpan t = TimeSpan.FromMilliseconds(FinishTime);
+                        string str = string.Format("{0:D2}:{1:D2}:{2:D3}", t.Minutes, t.Seconds, t.Milliseconds);
+                        byte[] IDs = File.ReadAllBytes(openFileDialog1.FileName).Skip(0x0).Take(4).ToArray();
+                        dataGridView1.Rows.Clear();
+                        string path = @"mkdd_input_reader_ghost.lua";
+                        using (StreamWriter sw = File.CreateText(path))
+                        {
+                            sw.WriteLine("local mkdd_input_reader_ghost = { -- { StickX(converted), StickY(converted), A, B, R, L, X, Z}");
+                            for (int i = 0; i <= Length; i += 2)
+                            {
+                                byte[] Inputs = File.ReadAllBytes(openFileDialog1.FileName).Skip(0x28 + i).Take(2).ToArray();
+                                var AnalogStickY = (Inputs[0] & 7);
+                                var AnalogStickX = Inputs[0] - AnalogStickY;
+                                byte ABLR = Inputs[1];
+                            dataGridView1.Rows.Add(i / 2
+                                , C.ConvertAnalogX(AnalogStickX)
+                                , C.ConvertAnalogY(AnalogStickY)
+                                , C.checkIfAPressed(Inputs[1])
+                                , C.checkIfBPressed(Inputs[1])
+                                , C.checkIfRPressed(Inputs[1])
+                                , C.checkIfLPressed(Inputs[1])
+                                , C.checkIfXPressed(Inputs[1])
+                                , C.checkIfZPressed(Inputs[1]));
+
+                            sw.WriteLine("{"
+                            + C.ConvertAnalogX(AnalogStickX) + ", "
+                            + C.ConvertAnalogY(AnalogStickY) + ", "
+                            + C.checkIfAPressed(Inputs[1]) + ", "
+                            + C.checkIfBPressed(Inputs[1]) + ", "
+                            + C.checkIfRPressed(Inputs[1]) + ", "
+                            + C.checkIfLPressed(Inputs[1]) + ", "
+                            + C.checkIfXPressed(Inputs[1]) + ", "
+                            + C.checkIfZPressed(Inputs[1]) + "},");
+                        }
+                            sw.WriteLine("}\nreturn mkdd_input_reader_ghost");
+                        }
+                        label3.Text = str;
+                        label4.Text = InputDataLength.ToString();
+                        label8.Text = C.GetCharInfo(IDs[0]);
+                        label9.Text = C.GetCharInfo(IDs[1]);
+                        label10.Text = C.GetKartID(IDs[2]);
+                        label11.Text = C.GetCourseID(IDs[3]);
+                }
+                else // inputdata array (.bin file), useful for when i forget to enable ghost saving
+                {
+                    ConverterFunctions C = new ConverterFunctions();
+                    string path = @"mkdd_input_reader_ghost.lua";
+                    using (StreamWriter sw = File.CreateText(path))
+                    {
+                        sw.WriteLine("local mkdd_input_reader_ghost = { -- { StickX(converted), StickY(converted), A, B, R, L, X, Z}");
+                        for (int i = 0; i < File.ReadAllBytes(openFileDialog1.FileName).Length; i += 2)
+                        {
+                            byte[] Inputs = File.ReadAllBytes(openFileDialog1.FileName).Skip(i).Take(2).ToArray();
+                            var AnalogStickY = (Inputs[0] & 7);
+                            var AnalogStickX = Inputs[0] - AnalogStickY;
+                            byte ABLR = Inputs[1];
+                            dataGridView1.Rows.Add(i / 2
+                                , C.ConvertAnalogX(AnalogStickX)
+                                , C.ConvertAnalogY(AnalogStickY)
+                                , C.checkIfAPressed(Inputs[1])
+                                , C.checkIfBPressed(Inputs[1])
+                                , C.checkIfRPressed(Inputs[1])
+                                , C.checkIfLPressed(Inputs[1])
+                                , C.checkIfXPressed(Inputs[1])
+                                , C.checkIfZPressed(Inputs[1]));
+
+                            sw.WriteLine("{"
+                            + C.ConvertAnalogX(AnalogStickX) + ", "
+                            + C.ConvertAnalogY(AnalogStickY) + ", "
+                            + C.checkIfAPressed(Inputs[1]) + ", "
+                            + C.checkIfBPressed(Inputs[1]) + ", "
+                            + C.checkIfRPressed(Inputs[1]) + ", "
+                            + C.checkIfLPressed(Inputs[1]) + ", "
+                            + C.checkIfXPressed(Inputs[1]) + ", "
+                            + C.checkIfZPressed(Inputs[1]) + "},");
+                        }
+                        sw.WriteLine("}\nreturn mkdd_input_reader_ghost");
+                    }
+                }
+
+
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
